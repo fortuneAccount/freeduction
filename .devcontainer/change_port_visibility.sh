085a13b49a4945fbcd79a402b7519c282df9b5b6
fortuneaccount <romjacket@outlook.com>
2026-09-09 20:03:34 -0700

    update

--- .devcontainer/change_port_visibility.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Change port visibility after codespace creation
-gh codespace ports visibility 6080:public -c $CODESPACE_NAME
-gh codespace ports visibility 5901:public -c $CODESPACE_NAME   
